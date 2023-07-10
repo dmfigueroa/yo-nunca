47d75a91b2bb563a5d40de0a8ddde80a68722fd0
David Figueroa <davidfigueroa0327@gmail.com>
2023-07-09 19:47:32 -0500

    messy commit

--- src/hooks/use-room.ts
+++ src/hooks/use-room.ts
@@ -1,10 +1,18 @@
 const useRoom = (name: string) => {
     const normalizedName = normalizeRoomName(name)
+
+
     return { name: normalizedName }
 };
 
 export const normalizeRoomName = (value: string) => {
     return value.replace(/[^a-zA-Z0-9]/g, "-").toLowerCase();
 };
+
+export const getOrCreateRoom = (name: string) => {
+    const normalizedName = normalizeRoomName(name)
+
+    
+}
 
 export default useRoom;
